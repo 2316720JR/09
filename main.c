6b829b37a1d55e78150de7587ad4fc7ef7e557c3
2316720JR <ljr2303@sookmyung.ac.kr>
2023-11-07 12:43:20 +0900

    project2

--- main.c
+++ main.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define N_STUDENT 5
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void)
 {
-	int grade[5];
 	int i;
+	int grade[N_STUDENT];
+	printf("enter 5 scores: \n");
 	
-	grade[0] = 10;
-	grade[1] = 20;
-	grade[2] = 30;
-	grade[3] = 40;
-	grade[4] = 50;
-	for (i=0; i<5; i++)
-	{
-	printf("grade[%d] = %d\n", i, grade[i]);
-	}
+	for(i=0; i<N_STUDENT; i++)
+		scanf("%d", &grade[i]);
+		
+	for(i=0; i<N_STUDENT; i++)
+		printf("grade[%d] = %d\n", i, grade[i]);
+		
 	return 0;
 }
